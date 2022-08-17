e9c639f59a17592298b5c68e20b3d87f8427fd44
Phill Legnitto <palegnitto@gmail.com>
2022-08-17 15:21:07 -0700

    created routes for mean, median, mode

--- app.js
+++ app.js
@@ -4,19 +4,42 @@ const express = require("express");
 const app = express();
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
+
+//stat functions
+const { findMean, findMode, findMedian } = require("./stats");
+
+// utility function to process query string
+const { convertStringToNumsArray } = require("./utils")
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
 
+app.get("/mean", function (req,res) {
+  const query = req.query.nums;
+  const conversion = convertStringToNumsArray(query);
+  const result = findMean(conversion);
+  return res.send({operation: "mean", result});
+})
+
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req,res) {
+  const query = req.query.nums;
+  const conversion = convertStringToNumsArray(query);
+  const result = findMedian(conversion);
+  return res.send({operation: "median", result});
+})
 
-
-/** Finds mode of nums in qs: returns {operation: "mean", result } */
-
+/** Finds mode of nums in qs: returns {operation: "mode", result } */
+app.get("/mode", function (req,res) {
+  const query = req.query.nums;
+  const conversion = convertStringToNumsArray(query);
+  const result = findMode(conversion);
+  return res.send({operation: "mode", result});
+})
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
--- utils.js
+++ utils.js
@@ -6,7 +6,21 @@ const { BadRequestError } = require("./expressError");
 function convertStrNums(strNums) {
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
+
+  const listOfNums = strNums.map(str => parseInt(str));
+  if (listOfNums.includes(NaN)){
+    throw new BadRequestError("Not all items could be converted to numbers");
+  }
+
+  return listOfNums;
+}
+
+/** Converts a string from our query to an array of nums */
+
+function convertStringToNumsArray(queryString) {
+  const numsGroup = queryString.split(",");
+  return convertStrNums(numsGroup)
 }
 
 
-module.exports = { convertStrNums };
+module.exports = { convertStringToNumsArray };
